d3c55566778bde79934f3ae157a7aa99ea7dc324
sp2005-im <sp2005im@gmail.com>
2024-12-03 17:41:42 +0530

    Problem: 50A (Domino Piling); Difficulty: 800; Tags: greedy, math

---
+++ 50A.cpp
@@ -0,0 +1,11 @@
+//Sri Rama Jayam
+//Problem: 50A (Domino Piling) Tags: greedy, math
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int m,n;
+	cin>>m>>n;
+	int n_dominoes = m*n/2;
+	cout<<n_dominoes<<endl;
+	return 0;
+}
