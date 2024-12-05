ef02a6ced08f811743f3790bbca3c96b77b82dd0
sp2005-im <sp2005im@gmail.com>
2024-12-05 16:54:14 +0530

    Problem : 630A (Another Twenty Five!) ; Difficulty : 800 ; Tag(s) : Number Theory

---
+++ 630A.cpp
@@ -0,0 +1,11 @@
+//Sri Rama Jayam
+//Problem: 630A Difficulty: 800 Tag(s): Number Theory
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int res = 25; //Last two digits of 5 raised to any power (exponent greater than or equal to two) is are 25
+	cout<<res<<endl;
+	return 0;
+}
